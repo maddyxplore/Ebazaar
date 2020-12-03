323e6863d19524b695a065d3f33dc0f289bf6f9d
Madhan Kumar <53962076+maddyexplore@users.noreply.github.com>
2020-12-03 14:02:05 +0530

    Add files via upload

---
+++ main/otp.py
@@ -0,0 +1,24 @@
+import random # for create random otp num
+import yagmail
+
+
+# generating the otp # 6 digits
+def otp():
+    otp_num = random.randint(100000,999999)
+    return otp_num
+
+
+def send_mail(reciever):
+    otp_num = str(otp())
+    yagmail.register('Ebazaar.web@gmail.com', 'madman2018')
+    receiver = reciever
+    body = "your otp is: "+otp_num
+
+    yag = yagmail.SMTP("Ebazaar.web@gmail.com")
+    yag.send(
+        to=receiver,
+        subject="Ebazaar otp",
+        contents=body, 
+    )
+    return otp_num
+
